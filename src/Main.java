239a61fc1a7d8d5be18be169c296710275a4569f
Jonathan Moraes <jonathan.pmoraes@senacsp.edu.br>
2021-12-25 18:32:07 -0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args){
+        AllFactors.printFactors(6);
+        AllFactors.printFactors(32);
+        AllFactors.printFactors(10);
+        AllFactors.printFactors(-1);
+    }
+}
